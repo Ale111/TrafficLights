c2b620c392377900a6d9c888ae6f58191d94dc98
Nick Dawson <nick@dawson.be>
2016-07-27 22:52:13 +1000

    Traffic Light FSM simulation

---
+++ TrafficLights/TrafficController.swift
@@ -0,0 +1,13 @@
+//
+//  TrafficController.swift
+//  TrafficLights
+//
+//  Created by Nick Dawson on 27/07/2016.
+//  Copyright © 2016 Nick Dawson. All rights reserved.
+//
+
+import UIKit
+
+class TrafficController: NSObject {
+
+}
